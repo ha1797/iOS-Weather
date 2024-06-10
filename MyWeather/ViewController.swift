7bdbf7908dd3061858ccbe9ea1d6609ecea8dcdc
Hoja Arzanesh <ha1797@rit.edu>
2024-06-10 15:24:19 -0400

    split up the project into 4 parts: location, table view, custom cell, api

--- MyWeather/ViewController.swift
+++ MyWeather/ViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+// Location: CoreLocation
+// table view
+// custom cell: collection view
+// API / request to get the data
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
